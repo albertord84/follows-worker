cbf61faca82a805ff1e46de8e90b773be4131e9d
josergm86 <josergm86@gmail.com>
2018-10-02 07:15:37 -0300

    a

--- worker/scripts/pro/check_payment_vindi.sh
+++ worker/scripts/pro/check_payment_vindi.sh
@@ -4,4 +4,4 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-curl http://localhost/follows/src/index.php/payment/check_payment_vindi >> /opt/lampp/htdocs/follows/worker/log/vindi/check-payment-vindi-${date}.log
+curl http://localhost/follows-worker/src/index.php/payment/check_payment_vindi >> /opt/lampp/htdocs/follows-worker/worker/log/vindi/check-payment-vindi-${date}.log
--- worker/scripts/pro/daily_report.sh
+++ worker/scripts/pro/daily_report.sh
@@ -4,4 +4,4 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-curl http://localhost/follows/worker/scripts/daily_report.php > /opt/lampp/htdocs/follows/worker/log/daily_report-${date}.log
+curl http://localhost/follows-worker/worker/scripts/daily_report.php > /opt/lampp/htdocs/follows-worker/worker/log/daily_report-${date}.log
