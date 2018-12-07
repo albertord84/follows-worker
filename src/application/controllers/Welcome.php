40061d90ceb8e480b8e4577d46e00eb291669573
Alberto Reyes <albertord84@gmail.com>
2018-12-07 17:24:20 -0200

    New Classes

--- src/application/controllers/Welcome.php
+++ src/application/controllers/Welcome.php
@@ -4,10 +4,16 @@ ini_set('xdebug.var_display_max_depth', 256);
 ini_set('xdebug.var_display_max_children', 256);
 ini_set('xdebug.var_display_max_data', 1024);
 
+
+
 class Welcome extends CI_Controller {
 
     public function index() {
         var_dump("olaaaa");
+        var_dump($_SERVER['DOCUMENT_ROOT']);
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/follows-worker/src/application/bussines/Client.php';
+        $C = new bussines\cls\Client(1);
+        $C->change_status(1);
     }
 
     public function test() {
