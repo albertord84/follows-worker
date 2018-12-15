fabfc92e9f2a4157f5f464997645d29d51c7527e
ruslangm88 <ruslan.guerra88@gmail.com>
2018-12-15 17:33:45 -0200

    alberto local

--- src/application/config/database.php
+++ src/application/config/database.php
@@ -74,7 +74,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$config = parse_ini_file(dirname(__FILE__) . "/../../../../FOLLOWS-WORKER.INI", true);
+$config = parse_ini_file(dirname(__FILE__)."/../../../../FOLLOWS-WORKER.INI", true);
 
 $db['default'] = array(
     'dsn' => '',
