1c35c9199f15a88f9a93d007b5759cff8c2c1bbd
iselaGit <imendozadec@gmail.com>
2018-12-18 18:30:07 -0200

    more updates

--- src/application/business/Worker/Worker.php
+++ src/application/business/Worker/Worker.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-namespace Worker
+namespace business\cls\worker
 {
     /**
      * Description of Worker
