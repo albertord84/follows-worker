f01b571481a41bf85d6ec650a394b529eb2fbcf0
CarlosH <cherreram2012@gmail.com>
2018-12-19 15:20:45 -0600

    todos los metodos del worker pasados

--- src/application/business/Worker/Worker.php
+++ src/application/business/Worker/Worker.php
@@ -28,28 +28,12 @@ namespace business\cls\worker {
      * ------------------------------------------
       +__construct($DB = NULL, $id = -1)
 
-      >>>>>> +get_worker_config() <<<<<<<<                      //NUEVA NO EXISTE
-      >>>>>> +request_follow_unfollow_work() <<<<<<<            //NUEVA NO EXISTE
-      >>>>>> +send_check_insta_user_work($Client) <<<<<<        //NUEVA NO EXISTE
-      >>>>>> +have_work() <<<<<<<<                              //NUEVA NO EXISTE
-      >>>>>> +get_work() : DailyWork <<<<<<<                    //NUEVA NO EXISTE
+      >>>>>> +get_worker_config() <<<<<<<<                      //EN ROJO EN EL MODELO
+      >>>>>> +request_follow_unfollow_work() <<<<<<<            //EN ROJO EN EL MODELO
+      >>>>>> +send_check_insta_user_work($Client) <<<<<<        //EN ROJO EN EL MODELO
+      >>>>>> +have_work() <<<<<<<<                              //EN ROJO EN EL MODELO
+      >>>>>> +get_work() : DailyWork <<<<<<<                    //EN ROJO EN EL MODELO
 
-
-      -prepare_daily_work($client_id =  NULL, $not_mail = false) : VOID
-
-      +do_client_work($daily_work) :BOOL    //ANTIGUA -> do_follow_unfollow_work
-
-      +save_follow_unfollow_work($Followeds_to_unfollow, $Ref_profile_follows, $daily_work) : BOOL
-
-      +get_work_by_id($reference_id) : DailyWork
-
-      +do_work($client_id = NULL, $n= NULL, $rp = NULL) :  VOID
-
-      +insert_daily_work($Ref_Prof, $to_follow, $to_unfollow, $login_data): VOID
-
-      +elete_daily_work($ref_prof_id) : VOID
-
-      +truncate_daily_work() : VOID
      */
 
     public $id;
