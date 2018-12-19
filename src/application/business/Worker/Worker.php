b93059554393884232118b47001a906ab39817e5
iselaGit <imendozadec@gmail.com>
2018-12-19 16:06:54 -0200

    worker

--- src/application/business/Worker/Worker.php
+++ src/application/business/Worker/Worker.php
@@ -24,19 +24,29 @@ namespace business\cls\worker
 +Robot
 +Gmail
          * ------------------------------------------
-        +__construct($DB = NULL, $id = -1)
-+get_worker_config() 
++__construct($DB = NULL, $id = -1)
+
+>>>>>> +get_worker_config() <<<<<<<<             //	NUEVA NO EXISTE
+>>>>>> +request_follow_unfollow_work() <<<<<<<   //     NUEVA NO EXISTE
+>>>>>> +send_check_insta_user_work($Client) <<<<<<  //	NUEVA NO EXISTE
+>>>>>> +have_work() <<<<<<<<  //   NUEVA NO EXISTE
+>>>>>> +get_work() : DailyWork <<<<<<<  //  NUEVA NO EXISTE
+
+
 -prepare_daily_work($client_id =  NULL, $not_mail = false) : VOID
-+request_follow_unfollow_work() 
-+do_follow_unfollow_work($daily_work) :BOOL
+
++do_client_work($daily_work) :BOOL    //ANTIGUA -> do_follow_unfollow_work
+
 +save_follow_unfollow_work($Followeds_to_unfollow, $Ref_profile_follows, $daily_work) : BOOL
-+send_check_insta_user_work($Client) 
-+have_work() 
-+get_work() : DailyWork 
+
 +get_work_by_id($reference_id) : DailyWork
+
 +do_work($client_id = NULL, $n= NULL, $rp = NULL) :  VOID
+
 +insert_daily_work($Ref_Prof, $to_follow, $to_unfollow, $login_data): VOID
-+delete_daily_work($ref_prof_id) : VOID
+
++elete_daily_work($ref_prof_id) : VOID
+
 +truncate_daily_work() : VOID
 */
         
